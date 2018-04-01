c91bdc9fcdfa5322347865a7d92d73436c7e7e98
peniu <37414449+peniu@users.noreply.github.com>
2018-04-01 19:54:49 +0200

    Add files via upload

--- punkt.java
+++ punkt.java
@@ -1,7 +1,6 @@
 public class punkt {
     //klasa z informacjami o pkt materialnym
     private double masa;
-    private double X,Y,Z;
     public void setMasa(double m){
         //akcesor z kontrola wartosci, masa nie moze byc ujemna ani zerowa
         if(m>0) {
@@ -13,25 +12,6 @@ public class punkt {
     }
     public double getMasa(){
         return masa;
-    }
-    //obsluga wspolrzednych punktu, nieuzywana
-    public void setX(double x){
-        X=x;
-    }
-    public void setY(double y){
-        X=y;
-    }
-    public void setZ(double z){
-        X=z;
-    }
-    public double getX(){
-        return X;
-    }
-    public double getY(){
-        return Y;
-    }
-    public double getZ(){
-        return Z;
     }
     public double moment_bezwladnosci(){
         int I=0;
@@ -45,25 +25,12 @@ public class punkt {
         System.out.println("Punkt materialny");
     }
     public punkt(double m){
-        //konstruktor umozliwiajacy podanie masy, wspolrzedne zerowe
+        //konstruktor umozliwiajacy podanie masy
         setMasa(m);
-        setX(0);
-        setY(0);
-        setZ(0);
     }
     public punkt(){
-        //konstruktor domyslny, masa rowna 1, wspolrzedne zerowe
+        //konstruktor domyslny, masa rowna 1,
         setMasa(1);
-        setX(0);
-        setY(0);
-        setZ(0);
-    }
-    public punkt(double m, double x, double y, double z){
-        //konstruktor umozliwiajacy podanie masy i wspolrzednych punktu
-        setMasa(m);
-        setX(x);
-        setY(y);
-        setZ(z);
     }
     public void info(){
     }
